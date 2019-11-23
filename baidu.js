be014f04ca0c5d519f3866b2ba1d60ef7c833f52
vipkid123 <lynn12999@163.com>
2019-11-23 16:41:15 +0800

    Delete baidu.js

--- baidu.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?56503e7cf574435a156330fabcbbe93b";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
